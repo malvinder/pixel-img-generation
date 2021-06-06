6456c19db6a4933daa2cc92cbd7b273fc2befd19
Malvinder Singh <dev.malvinder@gmail.com>
2021-06-06 19:44:55 +0530

    fixes for test cases check

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, { memo, useLayoutEffect, useRef } from 'react';
+import React, { memo, useLayoutEffect, useRef, useState } from 'react';
 import './App.css';
 
 const App = memo(() => {
   const height = 128;
   const width = 256;
   const canvasRef = useRef(null);
+  const [uniqueColors, setUniqueColors] = useState(0); // this is for testing purpose[]
 
   const getColor = (red, green, blue) => {
     return `rgb(${red},${green},${blue})`;
@@ -13,15 +14,29 @@ const App = memo(() => {
   const generateImage = () => {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext('2d');
+    const colors = [];
 
     for (let i=0; i<=255; i+=8) {
       for (let g=0; g<=255; g+=8) {
         for (let r=0; r<=255; r+=8) {
-          ctx.fillStyle = getColor(i, r, g);
-          ctx.fillRect(i, r, height, width);
+          colors.push(getColor(i, r, g));
         }
       }
     }
+
+    setUniqueColors(colors);
+
+    let w=0, h=0;
+    colors.forEach(uniqueColor => {
+      ctx.fillStyle = uniqueColor;
+      ctx.fillRect(w, h, 1, 1);
+      w++;
+
+      if (w > width) {
+        w = 0;
+        h++;
+      }
+    });
   };
 
   useLayoutEffect(() => {
@@ -29,6 +44,7 @@ const App = memo(() => {
   }, []);
   return (
     <div className="palette">
+      <input type="hidden" id="unique-colors" value={uniqueColors} />
       <canvas ref={canvasRef} width={width} height={height} />
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -1,19 +1,21 @@
-import { configure, shallow } from "enzyme";
+import { configure, shallow, mount } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import React, { useRef } from "react";
+import 'jest-canvas-mock';
 import App from "./App";
 
 configure({ adapter: new Adapter() });
 
 describe("rendering ColorPalette", () => {
-  it("should render my canvas with colors", () => {
-    const component = shallow(<App />);
-    expect(component.getElements()).toMatchSnapshot();
-  });
-
   it("should find the canvas element and match the given width and height", () => {
     const component = shallow(<App />);
     expect(component.find("canvas").props().width).toEqual(256);
     expect(component.find("canvas").props().height).toEqual(128);
+  });
+
+  it("should catch the color elements to be unique and 32768 in count", () => {
+    const wrapper = mount(<App />);
+    const arr = [...new Set(wrapper.find("#unique-colors").props().value)];
+    expect(arr.length).toEqual(32768);
   });
 });
